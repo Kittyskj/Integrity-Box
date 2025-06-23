4c554b8e00e631b590e0213b218b1c52a5adef70
Kitty <73357005+Kittyskj@users.noreply.github.com>
2025-06-23 19:20:28 +0300

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,10 @@
+pm uninstall meow.helper &>/dev/null
+rm -f /data/adb/tricky_store/keybox.xml
+rm -f /data/adb/tricky_store/keybox.xml.bak
+rm -f /data/adb/tricky_store/keybox.xml.md
+rm -f /data/adb/tricky_store/target.txt
+rm -f /data/adb/tricky_store/security_patch.txt
+rm -f /data/adb/tricky_store/tee_status
 rm -rf /data/adb/Integrity-Box-Logs
-rm -rf /data/adb/tricky_store/keybox.xml
-rm -rf /data/adb/tricky_store/keybox.xml.bak
-rm -rf /data/adb/tricky_store/target.txt
+rm -f /sdcard/stop
 rm -rf /data/adb/modules/integrity_box
